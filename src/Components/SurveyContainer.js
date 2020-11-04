b5300d88228dc1d853e712b05fe43fd58fae3de9
dposse <dposse.125@gmail.com>
2020-11-04 17:02:20 -0500

    Refactor to map through surveys

--- src/Components/SurveyContainer.js
+++ src/Components/SurveyContainer.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import IndividualSurvey from "./IndividualSurvey";
 import BorrowerInformationFormJSON from "../Surveys/BorrowerInformationForm.json";
 
 const SurveyContainer = () => {
-  return (
-    <IndividualSurvey surveyJSON={BorrowerInformationFormJSON} />
-  )
+  //for now importing JSON files and manually adding - I would love to figure out a way of getting all JSONs in a directory
+  // maybe shell script would be better for that
+  //  iterate through files in /Surveys, add import statement, add to useState?
+  const [surveysToDisplay] = useState([BorrowerInformationFormJSON]);
+
+  if (surveysToDisplay.length === 0) {
+    return <div>No surveys to display.</div>
+  }
+
+  return surveysToDisplay.map(survey => (
+    <IndividualSurvey surveyJSON={survey} />
+  ))
 }
 
 export default SurveyContainer;
